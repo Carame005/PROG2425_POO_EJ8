6b6469b1f721516bfe40981cab6ca690255bd10a
Caramelo <cccarlos@iesrafaelalberti.es>
2025-01-29 17:11:35 +0100

    Mejoras de codigo

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,66 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("HellDDDDo, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+
+class Libro(val titulo : String, val autor : String, val numPaginas : Int, val calificacion : Double){
+    init {
+        require(titulo.isNotEmpty()){"No puede haber un titulo vacío"}
+        require(autor.isNotEmpty()){"No puede haber un autor vacío"}
+        require(calificacion in 0..10){"La calificacion debe estar entre 0 y 10"}
     }
+}
+
+class ConjuntoLibros(){
+    val conjuntoLibros : Array<Libro?> = Array(5) { null }
+    fun añadirLibro(nuevoLibro : Libro) : Array<Libro?>{
+        var i = 0
+        if(i <= conjuntoLibros.size){
+            conjuntoLibros[i] = nuevoLibro
+            i += 1
+        }
+        return conjuntoLibros
+    }
+
+    /* TO DO FUNCION PARA RELLENAR LOS NULL CON LIBROS QUE NO EXISTAN*/
+    fun añadirLibrosImaginarios(){
+
+    }
+    fun eliminarLibro(titulo: String,autor: String){
+        for (libro in conjuntoLibros){
+            if (libro?.autor == autor || libro?.titulo == titulo){
+                libro = null
+            }
+        }
+    }
+    fun mostrarLibros() {
+        var menor: Libro
+        var mayor: Libro
+
+        for(libro in conjuntoLibros){
+            if (menor.calificacion > libro.calificacion){
+                if (libro != null) {
+                    menor = libro
+                }
+            }
+            if (mayor.calificacion < libro.calificacion){
+                if (libro != null) {
+                    mayor = libro
+                }
+            }
+        }
+        println("El libro con mayor calificacion tiene la siguiente descripcion: $mayor")
+        println("El libro con menor calificacion tiene la siguiente descripcion: $menor")
+    }
+    fun mostrarContenido(){
+        println(conjuntoLibros)
+    }
+}
+
+fun main() {
+    val libro1 = Libro("El moro", "Pepito Grillo",120,7.5)
+    val libro2 = Libro("La casa de mi abuela", "Manolo Cabeza de Bolo",180,4.3)
+    añadirLibro(libro1)
+    añadirLibro(libro2)
+    eliminarLibro(libro1.autor,libro1.titulo)
+    eliminarLibro(libro2.autor,libro2.titulo)
+    mostrarLibros()
+    mostrarContenido()
 }
